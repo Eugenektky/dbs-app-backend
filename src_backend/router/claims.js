c80a526076c4f932d1940c5966796d6f330c54ee
Robinho98 <rhlb98@gmail.com>
2023-02-25 15:16:04 +0800

    add insert operation

--- src_backend/router/claims.js
+++ src_backend/router/claims.js
@@ -4,7 +4,7 @@ const claimsRouter = new express.Router()
 var pool = require ("../config");
 
 claimsRouter.get('/view',function(req,res) {
-    // const claim = new Claims(req.body)
+    const employeeID = req
     pool.query("SELECT * FROM InsuranceClaims WHERE employeeId = req.employeeID", function (err, result) {
       if (err) throw err;
       else {
@@ -16,7 +16,6 @@ claimsRouter.get('/view',function(req,res) {
   })
 
 claimsRouter.get('/viewAll',function(req,res) {
-    // const claim = new Claims(req.body)
     pool.query("SELECT COUNT(ClaimID) FROM InsuranceClaims", function (err, result) {
       if (err) throw err;
       else {
@@ -28,8 +27,10 @@ claimsRouter.get('/viewAll',function(req,res) {
   })
 
 claimsRouter.get('/viewStatus',function(req,res) {
-    // const claim = new Claims(req.body)
-    pool.query("SELECT * FROM InsuranceClaims WHERE employeeId = req.employeeID AND status = req.status", function (err, result) {
+    const claimDetails = req.split(',')
+    const employeeID = claimDetails[0]
+    const status = claimDetails[1]
+    pool.query("SELECT * FROM InsuranceClaims WHERE employeeId = employeeID AND status = status", function (err, result) {
       if (err) throw err;
       else {
         res = result;
@@ -40,8 +41,33 @@ claimsRouter.get('/viewStatus',function(req,res) {
   })
 
 claimsRouter.get('/viewPurpose',function(req,res) {
-    // const claim = new Claims(req.body)
-    pool.query("SELECT * FROM InsuranceClaims WHERE employeeId = req.employeeID AND purpose = req.purpose", function (err, result) {
+    const claimDetails = req.split(',')
+    const employeeID = claimDetails[0]
+    const purpose = claimDetails[1]
+    pool.query("SELECT * FROM InsuranceClaims WHERE employeeId = employeeID AND purpose = purpose", function (err, result) {
+      if (err) throw err;
+      else {
+        res = result;
+        obj = {print: result};
+        console.log(obj);
+      }
+    })
+  })
+
+claimsRouter.post('/createClaim',function(req,res) {
+    const claimDetails = req.split(',')
+    const claimID = Math.random()
+    const insuranceID = claimDetails[0]
+    const firstName = claimDetails[1]
+    const lastName = claimDetails[2]
+    const expenseDate = claimDetails[3]
+    const amount = claimDetails[4]
+    const purpose = claimDetails[5]
+    const followUp = claimDetails[6]
+    const previousClaimID = claimDetails[7]
+    const status = 'Pending'
+    const lastEditedClaimDate = new Date().toJSON()
+    pool.query("INSERT INTO table_name VALUES (claimID, insuranceID, firstName, lastName, expenseDate, amount, purpose, followUp, previousClaimID, status, lastEditedClaimDate);", function (err, result) {
       if (err) throw err;
       else {
         res = result;
