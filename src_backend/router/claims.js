54ba55ceacd2060e2c6abce89d5f55e9f2038da0
Robinho98 <rhlb98@gmail.com>
2023-02-25 14:56:45 +0800

    add 4 get APIs

--- src_backend/router/claims.js
+++ src_backend/router/claims.js
@@ -4,8 +4,44 @@ const claimsRouter = new express.Router()
 var pool = require ("../config");
 
 claimsRouter.get('/view',function(req,res) {
-    const claim = new Claims(req.body)
-    pool.query("SELECT * FROM InsuranceClaims WHERE FirstName = claim.firstName AND LastName = claim.lastName", function (err, result) {
+    // const claim = new Claims(req.body)
+    pool.query("SELECT * FROM InsuranceClaims WHERE employeeId = req.employeeID", function (err, result) {
+      if (err) throw err;
+      else {
+        res = result;
+        obj = {print: result};
+        console.log(obj);
+      }
+    })
+  })
+
+claimsRouter.get('/viewAll',function(req,res) {
+    // const claim = new Claims(req.body)
+    pool.query("SELECT COUNT(ClaimID) FROM InsuranceClaims", function (err, result) {
+      if (err) throw err;
+      else {
+        res = result;
+        obj = {print: result};
+        console.log(obj);
+      }
+    })
+  })
+
+claimsRouter.get('/viewStatus',function(req,res) {
+    // const claim = new Claims(req.body)
+    pool.query("SELECT * FROM InsuranceClaims WHERE employeeId = req.employeeID AND status = req.status", function (err, result) {
+      if (err) throw err;
+      else {
+        res = result;
+        obj = {print: result};
+        console.log(obj);
+      }
+    })
+  })
+
+claimsRouter.get('/viewPurpose',function(req,res) {
+    // const claim = new Claims(req.body)
+    pool.query("SELECT * FROM InsuranceClaims WHERE employeeId = req.employeeID AND purpose = req.purpose", function (err, result) {
       if (err) throw err;
       else {
         res = result;
